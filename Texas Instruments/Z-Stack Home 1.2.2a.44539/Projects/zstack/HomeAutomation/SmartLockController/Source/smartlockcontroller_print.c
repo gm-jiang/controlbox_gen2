1455df92cf244e2402abb21e564e415fba779df7
jiangguangming <jiangguangming@meituan.com>
2016-01-08 15:56:34 +0800

    Create the smartlockcontroller source code
    this is the cc2530 z-stack 1.2.2a source code,we create the new project
    and add it.
    
    Change-Id: Ib0e3fe9480cbd5d9d3b8560aea3540003504db50
    Signed-off-by: jiangguangming <jiangguangming@meituan.com>

---
+++ Texas Instruments/Z-Stack Home 1.2.2a.44539/Projects/zstack/HomeAutomation/SmartLockController/Source/smartlockcontroller_print.c
@@ -0,0 +1,38 @@
+/***************************************************************************************************
+  Filename:       log_print.c
+
+***************************************************************************************************/
+
+/***************************************************************************************************
+ * INCLUDES
+ ***************************************************************************************************/
+
+#ifdef PRINT_DEBUG
+
+#include "hal_uart.h"
+#include "hal_types.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+#define BUF_SIZE 128
+void log_print( char *str )
+{
+    HalUARTWrite(uint8 port,uint8 * pBuffer,uint16 length)( HAL_UART_PORT_1, (uint8 *)str, strlen((const char *)str));
+}
+
+void log_printf(const char *fmt, ...)
+{
+    va_list args;
+    char log_buf[BUF_SIZE];
+
+    va_start(args, fmt);
+    vsprintf(log_buf,fmt,args);
+    log_print(log_buf);
+    va_end(args);
+}
+
+#endif
+
+/***************************************************************************************************
+***************************************************************************************************/
---
+++ Texas Instruments/Z-Stack Home 1.2.2a.44539/Projects/zstack/HomeAutomation/SmartLockController/Source/smartlockcontroller_print.h
@@ -0,0 +1,12 @@
+
+#ifdef PRINT_DEBUG
+
+#ifndef SMARTLOCKCONTROLLER_PRINT_H
+#define SMARTLOCKCONTROLLER_PRINT_H
+
+
+void log_print(char *str);
+void log_printf(const char *fmt, ...);
+#endif
+
+#endif
